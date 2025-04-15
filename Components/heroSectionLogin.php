7e0a81d7bb55eca4c0b8834a666bdbc2047a09e6
AniketKumar15 <110549582+AniketKumar15@users.noreply.github.com>
2025-04-15 20:28:54 +0530

    fix start reading btn Bug

--- Components/heroSectionLogin.php
+++ Components/heroSectionLogin.php
@@ -4,10 +4,15 @@
     <div class="hero-content">
       <h1><span>Think</span> <br> <span class="green-text">Code & Blog</span></h1>
       <p>A place to read, write, and Share your coding stories</p>
-
-      <div class="readBtn">
-        <a onclick="openModal()">Start Reading</a>
-      </div>
+      <?php if (!isset($_SESSION['username'])): ?>
+        <div class="readBtn">
+          <a onclick="openModal()">Start Reading</a>
+        </div>
+      <?php else: ?>
+        <div class="readBtn">
+          <a href="blogpage.php">Start Reading</a>
+        </div>
+      <?php endif; ?>
     </div>
     <div class="heroImg">
       <img src="./img/heroImg.png" alt="">
